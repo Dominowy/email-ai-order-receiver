9db4224cdcd71ec6c02ffa352979a749062604c0
Dominik Katana <dominik.kat@outlook.com>
2025-06-17 00:16:51 +0200

    create project with architecture and add entities

---
+++ Src/EAOR.Domain/Orders/Order.cs
@@ -0,0 +1,25 @@
+﻿using EAOR.Domain.Common;
+
+namespace EAOR.Domain.Orders
+{
+    public class Order : BaseEntity
+    {
+        public string ProductName { get; private set; }
+
+        public int Quantity { get; private set; }
+
+        public decimal Price { get; private set; }
+
+        protected Order(): base()
+        {
+
+        }
+
+        public Order(Guid id, string productName, int quantity, decimal price) : base(id)
+        {
+            ProductName = productName;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
